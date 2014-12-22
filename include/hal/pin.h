294227f5d695dfdb0bfe22d549918da3c806f87e
Frantisek Burian <BuFran@seznam.cz>
2014-12-23 00:52:46 +0100

    Add first version of doxygen documentation of PIN module

--- include/hal/pin.h
+++ include/hal/pin.h
@@ -14,10 +14,40 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+/** @defgroup PIN_module PIN manipulation module
+ *
+ * @brief PIN manipulation API
+ *
+ * @ingroup modules
+ *
+ * LGPL License Terms @ref lgpl_license
+ * 
+ * Basic example of usage:
+ * 
+ * @code
+ *  #include <hal/pin.h>
+ *  #define LED	PA8	// dependent on board
+ *  
+ *  int main(void)
+ *  {
+ *  	pin_clock_enable(LED);
+ *  	pin_set(LED, false);
+ *  	pin_output_pushpull(LED);
+ *  	
+ *  	while (true) {
+ *  		pin_toggle(LED);
+ *  	}
+ *  }
+ *  
+ * @endcode
+ *
+ */
 #ifndef HAL_PIN_H_INCLUDED
 #define HAL_PIN_H_INCLUDED
 
 #include <hal/common.h>
+
+/**@{*/
 
 /*****************************************************************************/
 /* API definitions                                                           */
@@ -29,34 +59,112 @@
 
 BEGIN_DECLS
 
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/**
+ * @defgroup PIN_api_clock PIN Peripheral Clock API
+ * @ingroup PIN_module
+ *
+ * @brief Pin Peripheral Clock manipulation
+ *
+ *@{*/
 static void pin_clock_enable(const uint32_t pin);
+/**@}*/
 
-/* pin value manipulation */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/**
+ * @defgroup PIN_api_opmode PIN State API
+ * @ingroup PIN_module
+ *
+ * @brief Pin state manipulation
+ *
+ *@{*/
+ 
+/*---------------------------------------------------------------------------*/
+/** @brief Get the actual pin state
+ *
+ * @param[in] pin pin name (@ref pin_name_base)
+ * @returns true, if pin is held high, false otherwise
+ */
 static bool pin_get(const uint32_t pin);
+
+/*---------------------------------------------------------------------------*/
+/** @brief Set the pin state
+ *
+ * @param[in] pin pin name (@ref pin_name_base)
+ * @param[in] val value to set 
+ */
 static void pin_set(const uint32_t pin, bool val);
+
+/*---------------------------------------------------------------------------*/
+/** @brief Toggle the pin level state
+ *
+ * The function creates edge on the specified pin.
+ *
+ * @param[in] pin pin name (@ref pin_name_base)
+ */
 static void pin_toggle(const uint32_t pin);
 
-/* pin pullup manipulation */
+/**@}*/
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/**
+ * @defgroup PIN_api_pullup PIN PullUp/PullDown API
+ * @ingroup PIN_module
+ *
+ * @brief Pin pullup manipulation
+ *
+ *@{*/
 static void pin_pull_disable(const uint32_t pin);
 static void pin_pull_down(const uint32_t pin);
 static void pin_pull_up(const uint32_t pin);
+/**@}*/
 
-/* pin direction manipulation */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/**
+ * @defgroup PIN_api_direction PIN Direction API
+ * @ingroup PIN_module
+ *
+ * @brief Pin direction manipulation
+ *
+ *@{*/
 static void pin_output_pushpull(const uint32_t pin);
 static void pin_output_opendrain(const uint32_t pin);
 static void pin_af_pushpull(const uint32_t pin);
 static void pin_af_opendrain(const uint32_t pin);
 static void pin_input(const uint32_t pin);
 static void pin_analog(const uint32_t pin);
+/**@}*/
 
-/* pin speed manipulation */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/**
+ * @defgroup PIN_api_speed PIN Speed API
+ * @ingroup PIN_module
+ *
+ * @brief Pin speed manipulation
+ *
+ *@{*/
 static void pin_speed_low(const uint32_t pin);
 static void pin_speed_medium(const uint32_t pin);
 static void pin_speed_fast(const uint32_t pin);
 static void pin_speed_high(const uint32_t pin);
+/**@}*/
 
-/* pin alternate mapping */
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/**
+ * @defgroup PIN_api_af PIN Alternate function API
+ * @ingroup PIN_module
+ *
+ * @brief Pin Alternate Function manipulation
+ *
+ *@{*/
 static void pin_af_map(const uint32_t pin, const uint32_t af);
+/**@}*/
 
 END_DECLS
 
@@ -83,5 +191,8 @@ END_DECLS
 #else
 # error "hal/pin.h have not defined your architecture."
 #endif
+
+
+/**@}*/
 
 #endif // HAL_PIN_H_INCLUDED
